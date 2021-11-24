a891fdbdb09fe32ee631767e5c5145246b59b7d3
Ayyanar N <lingam.ayyanar@gmail.com>
2021-11-23 21:37:03 -0600

    init commit for spring boot and react starter project

---
+++ src/main/java/no/ayyanarn/springreact/springreact/SpringreactApplication.java
@@ -0,0 +1,13 @@
+package no.ayyanarn.springreact.springreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringreactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/no/ayyanarn/springreact/springreact/SpringreactApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ayyanarn.springreact.springreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringreactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
